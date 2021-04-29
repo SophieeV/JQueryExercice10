e9bccdb9be7bd8aaeb0ce536b18777a6ede6ef5f
SophieeV <sophievesin@wanadoo.fr>
2021-04-29 10:32:38 +0200

    ajout fichiers

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $("#b1").click(function(){
+          //cache le texte
+          $('.a_cacher, #texte_3').hide();
+      });
+  });
+
+
+/*
+  $(document).ready(function(){
+     $("#texte_3").addClass("a_cacher");
+   });
+   $(function(){
+       $('.a_cacher').hide();
+     }); */
